a4328effaba9b77a1b75a0b4f501f8b78634bc5f
Aleksei Burov <burovalexprogrammer@gmail.com>
2023-08-25 20:30:53 +0200

    DAL init

---
+++ AspNetCore_Portfolio_DAL/Entity/Guest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebDAL.Entity
+{
+    //[Table("Guests")]
+    public class Guest
+    {
+        public Int64 id { get; set; }
+
+        [Required] //NOT_NULL
+        [MinLength(2)]
+        [MaxLength(20)]
+        public string name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string password { get; set; }
+
+        public bool checkIt;
+    }
+}
