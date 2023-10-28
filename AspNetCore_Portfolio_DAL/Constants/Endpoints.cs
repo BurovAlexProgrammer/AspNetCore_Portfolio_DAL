3f7a131f0d74d06a4649fc77cc4cb38c38b25a54
Aleksei Burov <burovalexprogrammer@gmail.com>
2023-10-28 09:54:47 +0200

    Add user policies

--- AspNetCore_Portfolio_DAL/Constants/Endpoints.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebClient.Constants
-{
-    public class Endpoints
-    {
-        public const string Login = "/Account/Login";
-        public const string Home = "/Home/Index";
-    }
-}
